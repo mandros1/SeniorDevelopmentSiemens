fb1d4d9d098603fd2668c11e492e87ef75fbf57c
Tim Endersby <endersbyt@gmail.com>
2019-02-07 11:53:33 -0500

    Remove unused DataTable code
    This code was not functional and only for testing

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -60,46 +60,7 @@ namespace SiemensPerformance
 
         private void Info_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            // Make the DataTable object.
-            DataTable dt = new DataTable("People");
-
-            // Add columns to the DataTable.
-            dt.Columns.Add("First Name",
-                System.Type.GetType("System.String"));
-            dt.Columns.Add("Last Name",
-                System.Type.GetType("System.String"));
-            dt.Columns.Add("Occupation",
-                System.Type.GetType("System.String"));
-            dt.Columns.Add("Salary",
-                System.Type.GetType("System.Int32"));
-
-            // Make all columns required.
-            for (int i = 0; i < dt.Columns.Count; i++)
-            {
-                dt.Columns[i].AllowDBNull = false;
-            }
-
-            // Make First Name + Last Name require uniqueness.
-            DataColumn[] unique_cols =
-            {
-        dt.Columns["First Name"],
-        dt.Columns["Last Name"]
-    };
-            dt.Constraints.Add(new UniqueConstraint(unique_cols));
-
-            // Add items to the table.
-            dt.Rows.Add(new object[]
-                {"Rod", "Stephens", "Nerd", 10000});
-            dt.Rows.Add(new object[]
-                {"Sergio", "Aragones", "Cartoonist", 20000});
-            dt.Rows.Add(new object[]
-                {"Eoin", "Colfer", "Author", 30000});
-            dt.Rows.Add(new object[]
-                {"Terry", "Pratchett", "Author", 40000});
-
-            // Make the DataGridView use the DataTable as its data source.
-
-            Info.DataContext = dt.DefaultView;
+           
         }
     }
 }
