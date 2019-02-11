ae448bdbabce5e9498d333181fc95a667d4f3b6a
Regina Locicero <rtl3971@rit.edu>
2019-02-11 00:02:16 -0500

    Added generating tabs upon startup and new tab creation code

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,41 +26,70 @@ namespace SiemensPerformance
 
         public MainWindow()
         {
+            
             InitializeComponent();
 
-            SeriesCollection CPU = new SeriesCollection
+            // Graph setup
+            /* SeriesCollection CPU = new SeriesCollection
+             {
+             new LineSeries
+             {
+                 Title = "CPU Usage",
+
+                 Values = new ChartValues<double> { 3, 5, 7, 4 },
+
+                 Fill = Brushes.Transparent,
+
+             },
+         };
+             RankGraph.Series = CPU;
+
+
+             SeriesCollection Memory = new SeriesCollection
+             {
+             new LineSeries
+             {
+                 Title = "Memory Usage",
+
+                 Values = new ChartValues<double> { 30, 5, 20, 4 },
+
+                 Fill = Brushes.Transparent,
+
+             },
+         };
+             RankGraph2.Series = Memory;*/
+
+            // Tabs setup
+
+            TabControl tabs = (TabControl)this.FindName("logNav");
+            // TODO: retrieve from backend
+            int numTabs = 3;
+            for(int i = 0; i < numTabs; i++)
             {
-            new LineSeries
-            {
-                Title = "CPU Usage",
-
-                Values = new ChartValues<double> { 3, 5, 7, 4 },
-
-                Fill = Brushes.Transparent,
-
-            },
-        };
-            RankGraph.Series = CPU;
-
-
-            SeriesCollection Memory = new SeriesCollection
-            {
-            new LineSeries
-            {
-                Title = "Memory Usage",
-
-                Values = new ChartValues<double> { 30, 5, 20, 4 },
-
-                Fill = Brushes.Transparent,
-
-            },
-        };
-            RankGraph2.Series = Memory;
+                TabItem tab = new TabItem();
+                tab.Header = "Log " + (i + 1).ToString();
+                tabs.Items.Insert(tabs.Items.Count - 1, tab);
+            }
         }
 
         private void Info_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
            
+        }
+
+        // Called when a new log tab item is chosen
+        private void LogNav_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TabControl control = sender as TabControl;
+            int currTab = control.SelectedIndex;
+            if(currTab == control.Items.Count-1)
+            {
+                TabItem tab = new TabItem();
+                tab.Header = "Log " + (control.Items.Count).ToString();
+                control.Items.Insert(control.Items.Count - 1, tab);
+                // TODO: Fix to select newly made tab
+                //control.SelectedIndex = control.Items.Count - 1;
+            }
         }
     }
 }
