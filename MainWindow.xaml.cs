f0b916ec3a7b9633c632b8a30518013dacaa071e
Tim Endersby <endersbyt@gmail.com>
2019-02-19 13:06:05 -0500

    Add tab dropbown menu
    Add rename ability
    Save As function
    
    BUGs - can't have more than one tab open

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,6 +14,9 @@ using LiveCharts.Wpf;
 using MahApps.Metro.Controls;
 using Microsoft.Win32;
 using Newtonsoft.Json;
+using Microsoft.VisualBasic;
+using System.Windows.Interactivity;
+using System.IO;
 
 namespace SiemensPerformance
 {
@@ -149,13 +152,92 @@ namespace SiemensPerformance
             //tab.Content = sv;
 
             //Convert JSON to DataGrid
-            DataSet dataSet = JsonConvert.DeserializeObject<DataSet>(data);
+            DataSet dataSet = JsonConvert.DeserializeObject<DataSet>(this.data);
             DataTable firstTable = dataSet.Tables[0];
             DataGrid dataGrid = new DataGrid();
             dataGrid.ItemsSource = firstTable.DefaultView;
             tab.Content = dataGrid;
 
+            //Tab dropdown menu
+            ContextMenu contextMenu = new ContextMenu();
+            MenuItem menuItem1 = new MenuItem();
+            contextMenu.Items.Add(menuItem1);
+            menuItem1.Header = "Rename";
+            menuItem1.Click += delegate { Rename(tab); };
+
+            MenuItem menuItem2 = new MenuItem();
+            contextMenu.Items.Add(menuItem2);
+            menuItem2.Header = "Save";
+            menuItem2.Click += delegate { Save(tab, this.data); };
+
+            MenuItem menuItem3 = new MenuItem();
+            contextMenu.Items.Add(menuItem3);
+            menuItem3.Header = "Save As...";
+            menuItem3.Click += delegate { SaveAs(tab, this.data); };
+
+            MenuItem menuItem4 = new MenuItem();
+            contextMenu.Items.Add(menuItem4);
+            menuItem4.Header = "Close";
+            menuItem4.Click += delegate { Close(tab); };
+
+            tab.ContextMenu = contextMenu;
+
             return tab;
+        }
+
+        private void Rename(TabItem tab)
+        {
+            string name = new InputBox("Name").ShowDialog();
+            if(name != "") {
+                tab.Header = name;
+            }
+        }
+
+        private void Save(TabItem tab, String json)
+        {
+            Console.WriteLine("Save dialaog");
+        }
+
+        private void SaveAs(TabItem tab, String json)
+        {
+            //set default file name to tab header
+            String defaultName = tab.Header.ToString();
+            //Remove .utr extention if present
+            if (defaultName.EndsWith(".utr"))
+            {
+                defaultName = defaultName.Substring(0, defaultName.Length - 4);
+            }
+
+            //Create save dialog
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.FileName = defaultName;
+            dlg.DefaultExt = ".json";
+            dlg.Filter = "Json files (.json)|*.json";
+
+            // Show save file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process save file dialog box results
+            if (result == true)
+            {
+                // Save document
+                string filename = dlg.FileName;
+                File.WriteAllText(filename, json);
+            }
+
+        }
+
+        private void Close(TabItem tab)
+        {
+            /*//TODO - fix this
+            if (tab != null)
+            {
+                // find the parent tab control
+                TabControl tabControl = tab.Parent as TabControl;
+
+                if (tabControl != null)
+                    tabControl.Items.Remove(tab); // remove tabItem
+            }*/
         }
     }
 }
